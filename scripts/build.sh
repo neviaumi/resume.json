3c8391728f26de2368b5fbbdc302da12b3769126
David Ng <david.ng.dev@gmail.com>
2021-03-25 23:24:48 +0800

    chore: message for skip-ci

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,5 @@ git config user.email "github-action@github.com"
 git config user.name "github-action[bot]"
 yarn build
 git add docs
-git commit -m "docs: publish new resume html"
+git commit -m "docs(release): publish new resume html[skip ci]"
 git push
