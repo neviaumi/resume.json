c93071461b9e476501d8755ff079c1821751ea7d
David Ng <david.ng.dev@gmail.com>
2021-03-25 23:48:25 +0800

    ci: remove existing html before create new one

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,8 @@ set -ex
 git config user.email "github-action@github.com"
 git config user.name "github-action[bot]"
 rm -f docs/index.html
+git add docs
+git commit -m "docs(release): remove existing resume html[skip ci]"
 yarn build
 git add docs
 git commit -m "docs(release): publish new resume html[skip ci]"
