db387b2d50901305d7ed953456b1a895b64392a3
David Ng <david.ng.dev@gmail.com>
2021-03-27 17:53:32 +0800

    fix: resume.pdf has unexcepted one more page

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,10 +4,9 @@ set -ex
 git config user.email "github-action@github.com"
 git config user.name "github-action[bot]"
 rm -f docs/index.html
-git add docs
-git commit -m "docs(release): remove existing resume html[skip ci]"
+rm -f docs/resume.pdf
 yarn build
 yarn build:pdf
 git add docs
-git commit -m "docs(release): publish new resume html[skip ci]"
-git push
+git commit -m "docs(release): publish new resume[skip ci]"
+git push || true
---
+++ theme/index.js
@@ -0,0 +1,18 @@
+const theme = require('jsonresume-theme-macchiato');
+
+/**
+ * Can use for print html also if https://github.com/jsonresume/resume-cli/pull/537 merged
+ * */
+module.exports = {
+  ...theme,
+  pdfRenderOptions: {
+    format: 'A4',
+    margin: {
+      bottom: 0,
+      left: 0,
+      right: 0,
+      top: 0,
+    },
+    mediaType: 'print',
+  },
+};
