f736ad66f946242a781ba64c3bf7cd54d8a5b8cd
David Ng <david.ng.dev@gmail.com>
2021-03-27 20:55:18 +0800

    fix: avoid build pdf in CI because result is unexcepted

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-set -ex
+set -x
 
-git config user.email "github-action@github.com"
-git config user.name "github-action[bot]"
-rm -f docs/index.html
-rm -f docs/resume.pdf
 yarn build
-yarn build:pdf
-git add docs
-git commit -m "docs(release): publish new resume[skip ci]"
-git push || true
+
+git diff --name-only --exit-code docs/index.html
+if [ $? -eq 1 ]; then
+  set -e
+  git config user.email "github-action@github.com"
+  git config user.name "github-action[bot]"
+  git add docs
+  git commit -m "docs(release): publish new resume[skip ci]"
+  git push
+else
+  echo "Result HTML not changed"
+fi
+
+
