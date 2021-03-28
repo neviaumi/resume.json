877eb32bac690611f4e34439cc06d17481481219
David Ng <david.ng.dev@gmail.com>
2021-03-28 19:53:25 +0800

    test: use latest version puppeteer

---
+++ buildPDF.js
@@ -0,0 +1,32 @@
+const puppeteer = require('puppeteer');
+const theme = require('./theme');
+const resume = require('./resume.json');
+
+async function main() {
+  const { render, pdfRenderOptions } = theme;
+
+  const html = await render(resume);
+  const browser = await puppeteer.launch({
+    args: [],
+  });
+  const page = await browser.newPage();
+
+  await page.emulateMediaType(pdfRenderOptions.mediaType);
+  await page.goto(
+    `data:text/html;base64,${Buffer.from(
+      unescape(encodeURIComponent(html)),
+    ).toString('base64')}`,
+    { waitUntil: 'networkidle0' },
+  );
+
+  await page.pdf({
+    format: 'A4',
+    path: 'docs/resume.pdf',
+    printBackground: true,
+    ...theme.pdfRenderOptions,
+  });
+
+  await browser.close();
+}
+
+main();
