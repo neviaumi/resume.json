4c21d5bff526efe2b25f6efef19fa30bb59ce003
David Ng <david.ng.dev@gmail.com>
2024-11-03 00:35:41 +0000

    GH-513: fix vite build
    this part of #513

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite';
 export default defineConfig(({ mode }) => {
   return {
     base: mode === 'development' ? '/' : '/resume.json',
+    esbuild: {
+      supported: {
+        'top-level-await': true,
+      },
+    },
     server: {
       host: '0.0.0.0',
       port: Number(process.env['PORT'] ?? '3000'),
