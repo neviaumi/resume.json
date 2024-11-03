cce8a8201fea35d26cb677ff44bb555c865bd282
David Ng <david.ng.dev@gmail.com>
2024-11-03 01:45:41 +0000

    GH-705: temp disabled pdf building on Github Action
    this part of #705

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,6 @@
 set -ex
 
 #touch ./public/resume.pdf
-node ./src/build-pdf.js
+#node ./src/build-pdf.js
 #docker compose -f docker-compose-build.yml up --build --exit-code-from pdf-builder --abort-on-container-exit
 npx vite build
