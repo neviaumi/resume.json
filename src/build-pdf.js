ef36f9e41a7c069bcb25069ff3ab3951c7abf3cf
David Ng <david.ng.dev@gmail.com>
2024-11-04 14:22:19 +0000

    GH-720: feedback to generated content before generate pdf
    this fix #720

--- src/build-pdf.js
+++ src/build-pdf.js
@@ -1,3 +1,5 @@
 import * as resumeToPdf from './resume-to-pdf.js';
 
-await resumeToPdf.generateResumeToPDF('public/resume.pdf');
+await resumeToPdf.generateResumeToPDF('public/KatKin.pdf', {
+  useTailoredResume: process.env['VITE_USE_TAILORED_RESUME'] ? true : false,
+});
