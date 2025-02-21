6fb4b27d67cb9181362af800c4ba83562ae9e625
David Ng <david.ng.dev@gmail.com>
2025-02-21 00:28:47 +0000

    GH-892: Refactor resume components and update build process.
    this close #892
    Replaced hardcoded resume links with dynamic slots for better flexibility and maintainability. Simplified the build script by commenting out unused commands. Updated the `.gitignore` to include `resume.pdf` and added improved styling for the resume component in `index.html`.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
 
-touch ./public/resume.pdf
-docker compose -f docker-compose-build.yml up --build --exit-code-from pdf-builder --abort-on-container-exit
+#touch ./public/resume.pdf
+#docker compose -f docker-compose-build.yml up --build --exit-code-from pdf-builder --abort-on-container-exit
 npx vite build
